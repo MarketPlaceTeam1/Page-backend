1e6ad0ad94d007bb3ef907a950749e5a02327087
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-30 21:02:09 +0200

    prueba cartas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 PORT = 3000;
 
+const fs = require ('fs')
 const Router = require("@koa/router");
 const Koa = require("koa");
 const { default: koaBody } = require("koa-body");
@@ -10,7 +11,7 @@ const app = new Koa();
 const router = new Router();
 
 
-const datosArray = []
+
 
 
 //----------------------------------------------------------------------------------
@@ -19,41 +20,41 @@ router.get("/", (ctx, next) => {
     ctx.status = 200;
 });
 
-router.post('/agregar-datos', (ctx) => {
+router.post('/Fronted', (ctx) => {
   const nuevosDatos = ctx.request.body;
-
-  if (!Array.isArray(nuevosDatos)) {
-  
-    nuevosDatos = [nuevosDatos];
-  }
-
-  
-  datosArray.length = 0;
-
-  
-  nuevosDatos.forEach(objeto => {
-    datosArray.push(objeto);
-  });
-
   ctx.body = { message: 'Datos agregados correctamente' };
 });
 
 
-router.post('/agregar-datosFormulario', (ctx) => {
-  const datos = ctx.request.body;
-  
-  datosArray.push(datos);
-    
-
-
-  ctx.body = { message: 'Datos agregados correctamente' };
-});
-
-router.get('/datos', (ctx) => {
+router.get('/Fronted', (ctx) => {
     ctx.response.type = 'application/json'; 
     ctx.response.body = datosArray; 
 });
 
+function guardarDatosFrontedEnArchivo(datosFronted) {
+  // Leer el contenido actual del archivo si existe
+  let datosExistentes = [];
+  try {
+    const archivoExistente = fs.readFileSync('datosFronted.json', 'utf-8');
+    datosExistentes = JSON.parse(archivoExistente);
+  } catch (error) {
+    // El archivo no existe o no se pudo leer, lo manejamos más adelante
+  }
+
+  // Agregar los nuevos datos a los datos existentes
+  datosExistentes.push(datosFronted);
+
+  // Guardar los datos en el archivo JSON
+  const datosJSON = JSON.stringify(datosExistentes, null, 2);
+
+  fs.writeFile('datosFronted.json', datosJSON, err => {
+    if (err) {
+      console.error('Error al guardar los datos de Fronted en el archivo JSON:', err);
+    } else {
+      console.log('Datos de Fronted guardados en el archivo JSON');
+    }
+  });
+}
 
 app.use(KoaLogger());
 app.use(cors({ 
