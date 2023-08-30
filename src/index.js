64b378be27258abb76df00681ef3b9a0fa73d652
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-30 22:01:53 +0200

    fix error

--- src/index.js
+++ src/index.js
@@ -1,58 +1,70 @@
-const fs = require('fs');
-const Router = require('@koa/router');
-const Koa = require('koa');
-const { default: koaBody } = require('koa-body');
-const KoaLogger = require('koa-logger');
-const cors = require('@koa/cors');
+import { access, constants, writeFile } from 'fs';
+import Router from '@koa/router';
+import Koa from 'koa';
+import { default as koaBody } from 'koa-body';
+import KoaLogger from 'koa-logger';
+import cors from '@koa/cors';
 const app = new Koa();
 
 const router = new Router();
 
-// Declaración de datosFronted
 const datosFronted = [];
 
-//----------------------------------------------------------------------------------
 router.get('/', (ctx, next) => {
   ctx.response.body = { message: 'Hello world!' };
   ctx.status = 200;
 });
 
-router.post('/Fronted', (ctx) => {
+router.post('/Fronted', async (ctx) => {
   const nuevosDatosFronted = ctx.request.body;
 
-  // Agregar los nuevos datos a datosFronted
-  datosFronted.push(nuevosDatosFronted);
+  try {
+    // Verificar si tienes permisos de escritura en la carpeta
+    await verificarPermisos();
 
-  // Guardar los datos en el archivo
-  guardarDatosFrontedEnArchivo();
+    // Agregar los nuevos datos a datosFronted
+    datosFronted.push(nuevosDatosFronted);
 
-  ctx.body = { message: 'Datos de Fronted agregados correctamente' };
+    // Guardar los datos en el archivo
+    await guardarDatosFrontedEnArchivo();
+
+    ctx.body = { message: 'Datos de Fronted agregados correctamente' };
+  } catch (error) {
+    console.error('Error al guardar los datos de Fronted:', error);
+    ctx.body = { message: 'Error al guardar los datos de Fronted' };
+  }
 });
 
-router.get('/Fronted', (ctx) => {
-  ctx.response.type = 'application/json';
-  ctx.response.body = datosFronted;
-});
+async function verificarPermisos() {
+  return new Promise((resolve, reject) => {
+    access(__dirname, constants.W_OK, (err) => {
+      if (err) {
+        reject('No tienes permisos de escritura en esta carpeta.');
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
-function guardarDatosFrontedEnArchivo() {
-  const datosJSON = JSON.stringify(datosFronted, null, 2);
+async function guardarDatosFrontedEnArchivo() {
+  return new Promise((resolve, reject) => {
+    const datosJSON = JSON.stringify(datosFronted, null, 2);
 
-  fs.writeFile('./datosFronted.json', datosJSON, err => {
-    if (err) {
-      console.error('Error al guardar los datos de Fronted en el archivo JSON:', err);
-    } else {
-      console.log('Datos de Fronted guardados en el archivo JSON');
-    }
+    writeFile('datosFronted.json', datosJSON, err => {
+      if (err) {
+        reject('Error al guardar los datos de Fronted en el archivo JSON');
+      } else {
+        console.log('Datos de Fronted guardados en el archivo JSON');
+        resolve();
+      }
+    });
   });
 }
 
 app.use(KoaLogger());
-app.use(cors({ 
-  origin: '*', 
-  methods: ["GET", "POST", "PUT"] 
-}));
+app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
 app.use(koaBody());
-
 app.use(router.routes()).use(router.allowedMethods());
 
 const PORT = 3000;
