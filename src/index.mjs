07d0fbfed7bd6518f40c021fc6fcf6c0b17bff38
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-30 23:09:15 +0200

    22

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,16 @@
-
+const fs = require('fs').promises;
+const path = require('path');
+const { promisify } = require('util');
 
 const Router = require('@koa/router');
 const Koa = require('koa');
 const { default: koaBody } = require('koa-body');
 const KoaLogger = require('koa-logger');
 const cors = require('@koa/cors');
-const path = require('path'); // Importa el módulo 'path'
 const app = new Koa();
-const fs = require('fs');
 
 const router = new Router();
 
-// Declaración de datosFronted
 const datosFronted = [];
 
 //----------------------------------------------------------------------------------
@@ -20,14 +19,13 @@ router.get('/', (ctx, next) => {
   ctx.status = 200;
 });
 
-router.post('/Fronted', (ctx) => {
+router.post('/Fronted', async (ctx) => {
   const nuevosDatosFronted = ctx.request.body;
 
-  // Agregar los nuevos datos a datosFronted
   datosFronted.push(nuevosDatosFronted);
 
   // Guardar los datos en el archivo
-  guardarDatosFrontedEnArchivo();
+  await guardarDatosFrontedEnArchivo();
 
   ctx.body = { message: 'Datos de Fronted agregados correctamente' };
 });
@@ -37,19 +35,16 @@ router.get('/Fronted', (ctx) => {
   ctx.response.body = datosFronted;
 });
 
-function guardarDatosFrontedEnArchivo() {
+async function guardarDatosFrontedEnArchivo() {
   const datosJSON = JSON.stringify(datosFronted, null, 2);
-  
-  // Obtener la ruta absoluta del archivo usando path.join
   const rutaArchivo = path.join(__dirname, './datosFronted.json');
 
-  fs.writeFile(rutaArchivo, datosJSON, err => {
-    if (err) {
-      console.error('Error al guardar los datos de Fronted en el archivo JSON:', err);
-    } else {
-      console.log('Datos de Fronted guardados en el archivo JSON');
-    }
-  });
+  try {
+    await fs.writeFile(rutaArchivo, datosJSON);
+    console.log('Datos de Fronted guardados en el archivo JSON');
+  } catch (error) {
+    console.error('Error al guardar los datos de Fronted en el archivo JSON:', error);
+  }
 }
 
 app.use(KoaLogger());
