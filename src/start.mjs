1820342f3a9d53a4528c4d7d36d55aa6f2f0891a
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-30 23:19:53 +0200

    22

--- src/start.mjs
+++ src/start.mjs
@@ -1,18 +1,19 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-
 async function deploy() {
-    const { stdout: output1 } = await exec("yarn install");
+  try {
+    const { stdout: output1 } = await execPromise("npm install");
     console.log("Installing dependencies...");
     console.log(output1);
 
-    const { stdout: output2 } = await exec("yarn db:migrate");
+    const { stdout: output2 } = await execPromise("npm run db:migrate");
     console.log("Migrating database...");
     console.log(output2);
 
-    const { stdout: output3 } = await exec("yarn db:seed");
+    const { stdout: output3 } = await execPromise("npm run db:seed");
     console.log("Seeding database...");
     console.log(output3);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
 deploy();
