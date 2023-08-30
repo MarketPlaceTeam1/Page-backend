46687bef1ccbd94082ae65502d5a5ae7220502f2
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-30 22:22:51 +0200

    fix error

--- src/start.js
+++ src/start.js
@@ -1,25 +1,23 @@
 import { promisify } from "util";
-import { exec } from "child_process";  // Importar directamente de child_process
+import { exec } from "child_process";
 const execPromise = promisify(exec);
 
 async function deploy() {
   try {
-    const { stdout: output1 } = await execPromise("yarn install");
+    const { stdout: output1 } = await execPromise("npm install");
     console.log("Installing dependencies...");
     console.log(output1);
 
-    const { stdout: output2 } = await execPromise("yarn db:migrate");
+    const { stdout: output2 } = await execPromise("npm run db:migrate");
     console.log("Migrating database...");
     console.log(output2);
 
-    const { stdout: output3 } = await execPromise("yarn db:seed");
+    const { stdout: output3 } = await execPromise("npm run db:seed");
     console.log("Seeding database...");
     console.log(output3);
   } catch (error) {
     console.error("An error occurred:", error);
   }
 }
-
-deploy();
 
 deploy();
